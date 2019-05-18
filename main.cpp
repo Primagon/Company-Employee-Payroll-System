f51e28d640eb3cce657a04beafbbb6baa4656ed4
Ravid Rahman <ravidrahman@gmail.com>
2019-05-18 17:24:48 -0400

    got department head working

--- main.cpp
+++ main.cpp
@@ -16,9 +16,49 @@ void populateTree(CEO boss);
 int main(){
 
     CEO boss('R', "Giovanni", 1, 200000);
-    populateTree(boss);
 
-    Employee bro = boss.getDepartmentH()[1].getManagers()[2].getEmployees()[4];
+
+
+
+    ifstream myfile;
+    myfile.open("randLastName.txt");
+    srand(time(NULL));
+    vector<string> names;
+    int a = 0;
+    string line;
+
+    if(myfile.is_open()){
+        while(getline (myfile,line)){
+            names.push_back(line);
+        }
+        myfile.close();
+    }
+
+    for(int i=1; i<=2; i++){
+        int tempID = boss.GetEmployeeID()*10;
+        DepartmentH tempDepartmentH((char)(rand()%26+65), names[rand()%61], tempID+i, 150000);
+        boss.addDepartmentH(tempDepartmentH);
+        for(int j=1; j<=3; j++){
+            tempID = boss.getDepartmentH()[i-1].GetEmployeeID()*10;
+            Manager tempManager((char)(rand()%26+65), names[rand()%61], tempID+j, 100000);
+            boss.getDepartmentH()[i-1].addManager(tempManager);
+            for(int k=1; k<=5; k++){
+                tempID = boss.getDepartmentH()[i-1].getManagers()[j-1].GetEmployeeID()*10;
+                Employee tempEmployee((char)(rand()%26+65), names[rand()%61], tempID+k, 75000);
+                boss.getDepartmentH()[i-1].getManagers()[j-1].addEmployee(tempEmployee);
+            }
+        }
+    }
+
+
+
+
+
+
+
+    //populateTree(boss);
+
+    DepartmentH bro = boss.getDepartmentH()[0];
 
     cout << bro.GetName() << endl;
     cout << bro.GetEmployeeID() << endl;
@@ -46,13 +86,15 @@ void populateTree(CEO boss){
 
     for(int i=1; i<=2; i++){
         int tempID = boss.GetEmployeeID()*10;
-        boss.addDepartmentH(DepartmentH((char)(rand()%26+65), names[rand()%61], tempID+i, 150000));
+        DepartmentH temp((char)(rand()%26+65), names[rand()%61], tempID+i, 150000);
+        boss.addDepartmentH(temp);
+        cout << boss.getDepartmentH()[i-1].GetEmployeeID() << endl;
         for(int j=1; j<=3; j++){
-            tempID = boss.getDepartmentH()[i].GetEmployeeID()*10;
-            boss.getDepartmentH()[i].addManager(Manager((char)(rand()%26+65), names[rand()%61], tempID+j, 100000));
+            tempID = boss.getDepartmentH()[i-1].GetEmployeeID()*10;
+            boss.getDepartmentH()[i-1].addManager(Manager((char)(rand()%26+65), names[rand()%61], tempID+j, 100000));
             for(int k=1; k<=5; k++){
-                tempID = boss.getDepartmentH()[i].getManagers()[j].GetEmployeeID()*10;
-                boss.getDepartmentH()[i].getManagers()[j].addEmployee(Employee((char)(rand()%26+65), names[rand()%61], tempID+k, 75000));
+                tempID = boss.getDepartmentH()[i-1].getManagers()[j-1].GetEmployeeID()*10;
+                boss.getDepartmentH()[i-1].getManagers()[j-1].addEmployee(Employee((char)(rand()%26+65), names[rand()%61], tempID+k, 75000));
             }
         }
     }
