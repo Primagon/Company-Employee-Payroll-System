6487d0d73d56e4ce8bea11c1b84a4349bc2f589a
anthonyzhu137 <anthonyzhu137@gmail.com>
2019-05-18 23:20:11 -0400

    edits

--- main.cpp
+++ main.cpp
@@ -17,9 +17,6 @@ int main(){
 
     CEO boss('R', "Giovanni", 1, 200000);
 
-
-
-
     ifstream myfile;
     myfile.open("randLastName.txt");
     srand(time(NULL));
@@ -43,27 +40,31 @@ int main(){
             Manager tempManager((char)(rand()%26+65), names[rand()%61], tempID+j, 100000);
             boss.getDepartmentH()[i-1].addManager(tempManager);
             for(int k=1; k<=5; k++){
-                tempID = boss.getDepartmentH()[i-1].getManagers()[j-1].GetEmployeeID()*10;
+                tempID*=10;
                 Employee tempEmployee((char)(rand()%26+65), names[rand()%61], tempID+k, 75000);
-                boss.getDepartmentH()[i-1].getManagers()[j-1].addEmployee(tempEmployee);
+                DepartmentH testing = boss.getDepartmentH()[0];
+                Manager test = testing.getManagers()[0];
+                test.addEmployee(tempEmployee); 
             }
         }
     }
-
-
-
-
-
-
-
+    
     //populateTree(boss);
 
     DepartmentH bro = boss.getDepartmentH()[0];
+    DepartmentH boi = boss.getDepartmentH()[1];
+    //Manager hi = boss.getDepartmentH()[0].getManagers()[0];
 
     cout << bro.GetName() << endl;
-    cout << bro.GetEmployeeID() << endl;
+    //cout << bro.GetEmployeeID() << endl;
     cout << bro.GetContactInfo() << endl;
     cout << bro.GetGrossPay() << endl;
+
+    cout << boi.GetName() << endl;
+    //cout << boi.GetEmployeeID() << endl;
+    cout << boi.GetContactInfo() << endl;
+    cout << boi.GetGrossPay() << endl;
+
 
     return 0;
 }
